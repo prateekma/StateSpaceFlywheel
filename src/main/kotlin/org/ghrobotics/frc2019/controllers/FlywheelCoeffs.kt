fc911f814c5a6a9c936086a33a70936d2281ad22
Prateek Machiraju <prateek.machiraju@gmail.com>
2019-05-26 11:15:24 -0400

    Update FalconLibrary

--- src/main/kotlin/org/ghrobotics/frc2019/controllers/FlywheelCoeffs.kt
+++ src/main/kotlin/org/ghrobotics/frc2019/controllers/FlywheelCoeffs.kt
@@ -1,24 +1,37 @@
 package org.ghrobotics.frc2019.controllers
 
-import koma.end
-import koma.mat
+import org.ghrobotics.lib.mathematics.linalg.*
 
 object FlywheelCoeffs {
 
-    val A = mat[0.997502463097695, 0.6216972081698791 end
-                0.0, 0.0]
+    val A = mat(`2`, `2`).fill(
+        0.997502463097695, 0.6216972081698791,
+        0.000000000000000, 0.0000000000000000
+    )
 
-    val B = mat[0.6155494833981922 end
-                0.0]
+    val B = mat(`2`, `1`).fill(
+        0.6155494833981922,
+        0.0000000000000000
+    )
 
-    val C = mat[1.0, 0.0]
+    val C = mat(`1`, `2`).fill(
+        1.0, 0.0
+    )
 
-    val D = mat[0.0]
+    val D = mat(`1`, `1`).fill(
+        0.0
+    )
 
-    val K = mat[0.8654801449595761, 1.0]
+    val K = mat(`1`, `2`).fill(
+        0.8654801449595761, 1.0
+    )
 
-    val Kff = mat[0.6185642626159451, 0.0]
+    val Kff = mat(`1`, `2`).fill(
+        0.6185642626159451, 0.0
+    )
 
-    val L = mat[0.999900019945138 end
-                0.0]
+    val L = mat(`2`, `1`).fill(
+        0.999900019945138,
+        0.000000000000000
+    )
 }
--- src/main/kotlin/org/ghrobotics/frc2019/controllers/FlywheelController.kt
+++ src/main/kotlin/org/ghrobotics/frc2019/controllers/FlywheelController.kt
@@ -1,38 +1,32 @@
 package org.ghrobotics.frc2019.controllers
 
-import koma.extensions.get
-import koma.extensions.set
-import koma.mat
-import koma.zeros
+import org.ghrobotics.lib.mathematics.linalg.*
 import org.ghrobotics.lib.mathematics.statespace.*
 
 class FlywheelController {
 
-    private val observerCoefficients = StateSpaceObserverCoeffs(2, 1, 1) { FlywheelCoeffs.L }
-    private val plantCoefficients = StateSpacePlantCoeffs(2, 1, 1) {
-        listOf(
-            FlywheelCoeffs.A,
-            FlywheelCoeffs.B,
-            FlywheelCoeffs.C,
-            FlywheelCoeffs.D
-        )
-    }
-    private val controllerCoefficients = StateSpaceControllerCoeffs(2, 1, 1) {
-        listOf(
-            FlywheelCoeffs.K,
-            FlywheelCoeffs.Kff,
-            mat[-12.0],
-            mat[+12.0]
-        )
-    }
+    private val observerCoefficients = StateSpaceObserverCoeffs<`2`, `1`, `1`>(FlywheelCoeffs.L)
+
+    private val controllerCoefficients = StateSpaceControllerCoeffs<`2`, `1`, `1`>(
+        FlywheelCoeffs.K,
+        FlywheelCoeffs.Kff,
+        vec(`1`).fill(-12.0),
+        vec(`1`).fill(+12.0)
+    )
+
+    private val plantCoefficients = StateSpacePlantCoeffs(
+        `2`, `1`, `1`,
+        FlywheelCoeffs.A, FlywheelCoeffs.B, FlywheelCoeffs.C, FlywheelCoeffs.D
+    )
+
 
     private val plant = StateSpacePlant(plantCoefficients)
     private val controller = StateSpaceController(controllerCoefficients, plant)
     private val observer = StateSpaceObserver(observerCoefficients, plant)
 
-    private var reference = zeros(2, 1)
-    private var y = zeros(1, 1)
-    private var u = mat[0.0]
+    private var reference = vec(`2`).fill(0.0, 0.0)
+    private var y = vec(`1`).fill(0.0)
+    private var u = vec(`1`).fill(0.0)
 
     /**
      * Returns the input voltage in Volts
